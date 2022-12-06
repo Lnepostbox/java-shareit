ca2f61bb2ef90ec344d83172e572d22e0427bd4f
Elena <lnepostbox@yandex.ru>
2022-12-07 00:58:14 +0300

    feat: add-item-requests v0

--- src/main/java/ru/practicum/shareit/item/dto/ItemDtoResponse.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDtoResponse.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ItemDtoResponse {
 
     private Long id;
---
+++ src/test/java/ru/practicum/shareit/bookingTest/BookingDtoTest.java
@@ -0,0 +1,35 @@
+package ru.practicum.shareit.bookingTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.boot.test.json.JsonContent;
+import ru.practicum.shareit.booking.dto.BookingDtoResponse;
+import java.time.LocalDateTime;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@JsonTest
+public class BookingDtoTest {
+    @Autowired
+    JacksonTester<BookingDtoResponse> json;
+
+    @Test
+    void testBookingDto() throws Exception {
+        BookingDtoResponse bookingDto = new BookingDtoResponse(
+                1L,
+                LocalDateTime.of(2022, 12, 12, 10, 10, 1),
+                LocalDateTime.of(2022, 12, 20, 10, 10, 1),
+                null,
+                null,
+                null);
+
+        JsonContent<BookingDtoResponse> result = json.write(bookingDto);
+
+        assertThat(result).extractingJsonPathNumberValue("$.id").isEqualTo(1);
+        assertThat(result).extractingJsonPathStringValue("$.start")
+                .isEqualTo(LocalDateTime.of(2022, 12, 12, 10, 10, 1).toString());
+        assertThat(result).extractingJsonPathStringValue("$.end")
+                .isEqualTo(LocalDateTime.of(2022, 12, 20, 10, 10, 1).toString());
+    }
+}
